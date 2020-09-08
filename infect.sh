ead7f392c59664aedf2933a2f22b85178cdbe847
Daniel Pavela <daniel.pavela@gmail.com>
2020-09-08 22:08:25 +1000

    Implement installation script

--- infect.sh
+++ infect.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
-chmod +x ./payload/main.sh
-./payload/main.sh
+
+# Warn user
+echo "CommunistVirus - A port of the communist virus to Linux!"
+echo -e "\033[31;5;1;7mWARNING! I AM NOT RESPONSIBLE FOR ANY DAMAGE CAUSED TO YOUR DEVICES\033[0m"
+
+read -p "Continue? (y/N) " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	# Make directory, git clone to directory
+	mkdir ~/.communistVirus/
+	git clone https://github.com/daniel071/communistVirusLinux.git ~/.communistVirus/
+
+	# Make backup of profile file
+	cp ~/.profile ./profile_backup
+
+	# Add to auto start up
+	echo "~/.communistVirus/payload/main.sh"
+
+	# epic, it is done
+	echo -e "\033[1;32mInstallation was successfull! \033[0m"
+fi
